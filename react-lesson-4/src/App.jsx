96f5347fda440f6e1a0f0aecfc0f83a92bc97706
Muhammadimin0011-ship-it <muhademinmamateminov@gmail.com>
2026-07-28 12:28:27 +0500

    App.jsx

--- react-lesson-4/src/App.jsx
+++ react-lesson-4/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [like, setLike] = useState(0)
+  const [name, setName] = useState("Ali")
 
   function heandleNext() {
     setCount(count + 1)
@@ -12,12 +14,36 @@ function App() {
     setCount(count - 1)
   }
 
+  function plusLike() {
+    setLike(like + 1)
+  }
+
+  function almashtirish() {
+    setName("Vali")
+  }
+
   return (
     <div>
-      <button onClick={heandleNext}>Increment</button>
-      <button onClick={heandlePrev}>Decrement</button>
-      <h1>Counter: {count} </h1>
+      <div>
+        <h1>birinchi vazifa</h1>
+        <button onClick={heandleNext}>Increment</button>
+        <button onClick={heandlePrev}>Decrement</button>
+        <h1>Counter: {count} </h1>
+      </div>
+
+      <div>
+        <h1>Ikkinchi vazifa</h1>
+        <button onClick={plusLike}>like</button>
+        <h1>Likes: {like}</h1> 
+      </div>
+
+      <div>
+        <h1>Uchunchi vazifa</h1>
+        <button onClick={almashtirish}>Almashtirish</button>
+        <h1>Name: {name}</h1>
+      </div>
     </div>
+
   )
 }
 
