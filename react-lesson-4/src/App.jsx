0a2f5b2bfadbe2da7b014446907a96b86a6a2c00
Muhammadimin0011-ship-it <muhademinmamateminov@gmail.com>
2026-07-28 12:16:38 +0500

    App.jsx

--- react-lesson-4/src/App.jsx
+++ react-lesson-4/src/App.jsx
@@ -5,8 +5,17 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  function heandleNext() {
+    setCount(count + 1)
+  }
+  function heandlePrev() {
+    setCount(count - 1)
+  }
+
   return (
     <div>
+      <button onClick={heandleNext}>Increment</button>
+      <button onClick={heandlePrev}>Decrement</button>
       <h1>Counter: {count} </h1>
     </div>
   )
