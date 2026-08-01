125379b237ea06dbecaaecc9a3fca700c386be36
Muhammadimin0011-ship-it <muhademinmamateminov@gmail.com>
2026-08-01 15:24:40 +0500

    App.jsx

--- react-lesson-7/src/App.jsx
+++ react-lesson-7/src/App.jsx
@@ -1,15 +1,65 @@
-import { useState } from 'react'
+const getVideos = () => {
+  console.log("Fetching videos...");
+
+  return [
+    {
+      videoImg: "cjdj", title: "dasturlash"
+    },
+    {
+      videoImg: "cjdj", title: "dasturlash"
+    },
+    {
+      videoImg: "cjdj", title: "dasturlash"
+    },
+    {
+      videoImg: "cjdj", title: "dasturlash"
+    },
+  ]
+
+}
+
+
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState("")
+  const [video, setvideo] = useState([])
+
+  const vidie = getVideos()
+
+
+  useEffect(() => {
+    getVideos()
+  }, [])
+
 
   return (
     <>
-      <h1>
-        hello acer
-      </h1>
+      <div>
+        <input onChange={(e) => { setCount(e.target.value) }} type="text" placeholder='search..' />
+
+
+        {
+          vidie.map((item, index, rray) => {
+
+            if (item.title.toLowerCase().includes(count.toLowerCase())) {
+            return (
+
+              <div>
+                <img src={item.img} alt="" />
+
+                <h1>{item.title}</h1>
+              </div>
+
+            )
+          }
+          }
+
+      )
+        }
+      </div >
     </>
   )
 }
---
+++ react-lesson-8/src/App.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const [showProfile, setshowProfile] = useState(true)
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    console.log("Theme changed.");
+  }, [darkMode]);
+
+  function changeTheme() {
+    setDarkMode(!darkMode);
+  }
+
+
+  useEffect(() => {
+    if (showProfile) {
+      console.log("Profile ko'rsatildi.");
+    } else {
+      console.log("Profile yashirildi.");
+    }
+  }, [showProfile]);
+
+  function toggleProfile() {
+    setshowProfile(!showProfile);
+  }
+
+  if (!showProfile) {
+    return (
+      <div className="container">
+        <button onClick={toggleProfile}>Show Profile</button>
+      </div>
+    );
+  }
+
+
+  useEffect(() => {
+    document.title = name;
+  }, [name]);
+
+
+  return (
+    <div>
+
+      <div className={darkMode ? "dark" : "light"}>
+        <h1>{darkMode ? "🌙 Dark Mode" : "🌞 Light Mode"}</h1>
+
+        <button onClick={changeTheme}>
+          {darkMode ? "Light Mode" : "Dark Mode"}
+        </button>
+      </div>
+
+      <div className="container">
+        <button onClick={toggleProfile}>Hide Profile</button>
+
+        <div className="card">
+          <h1>👤</h1>
+          <h2>Ali Valiyev</h2>
+          <p>Frontend Developer</p>
+          <p>📍 Jizzax</p>
+        </div>
+      </div>
+
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Ismingizni kiriting"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <h1>Salom, {name}!</h1>
+      </div>
+
+      
+    </div>
+
+
+  )
+}
+
+export default App
