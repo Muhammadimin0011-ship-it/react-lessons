b70da7e1c2596f8242445bcb42482aa1c3d82dfe
Muhammadimin0011-ship-it <muhademinmamateminov@gmail.com>
2026-08-06 11:54:32 +0500

    App.jsx

--- react-lesson-12/src/App.jsx
+++ react-lesson-12/src/App.jsx
@@ -1,121 +1,101 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState([])
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [search, setSearch] = useState("")
+
+
+  function addBook() {
+    if (title === "" || author === "") {
+      return alert('xamma malumotlarni toldiring')
+    }
+    const newBook = {
+      title: title,
+      author: author,
+      completed: false,
+    };
+
+    setBooks([...books, newBook])
+
+    setTitle("")
+    setAuthor("")
+
+
+  }
+
+  function toggleComplete(index) {
+    const updatedBooks = books.map((item, itemIndex) => {
+      if (itemIndex === index) {
+        return {
+          ...item,
+          completed: !item.completed,
+        };
+      }
+
+      return item;
+    });
+
+    setBooks(updatedBooks);
+  }
+
+  function heandleDelete(index) {
+    const deleteItem = books.filter((item, itemindex) => itemindex !== index);
+
+    setBooks(deleteItem)
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <input
+        type="text"
+        placeholder="Kitob nomi"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
 
-      <div className="ticks"></div>
+      <input
+        type="text"
+        placeholder="Muallif"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <button onClick={addBook}>Add book</button>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <hr />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+
+      <input
+        type="text"
+        placeholder="Qidirish..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+
+
+      <div>
+        {books.filter((item) => {
+          return item.title.toLowerCase().includes(search.toLowerCase());
+        }).map((item, index) => (
+          <div key={index}>
+            <h3>📖 {item.title}</h3>
+
+            <p>👤 {item.author}</p>
+
+            <p>
+              Status: {item.completed ? "O'qildi" : "O'qilmagan"}
+            </p>
+
+            <button onClick={() => toggleComplete(index)}>✅ O'qildi</button>
+
+            <button onClick={() => { heandleDelete(index) }}>🗑 O'chirish</button>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
